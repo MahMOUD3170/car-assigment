80d9d1b9b478819c6b682e3c5e0f0107271e2250
miko200000 <m.khaled3170@gmail.com>
2019-09-27 23:43:51 +0200

    app libraries

---
+++ return_std.h
@@ -0,0 +1,15 @@
+/*
+ * return_std.h
+ *
+ *  Created on: ??þ/??þ/????
+ *      Author: COMPUTER SHOW
+ */
+
+#ifndef RETURN_STD_H_
+#define RETURN_STD_H_
+#include"manual_std_types.h"
+
+#define OK (uint8)0x01
+#define NOK (uint8)0x00
+
+#endif /* RETURN_STD_H_ */
